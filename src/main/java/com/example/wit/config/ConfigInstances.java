9ce3ff0934301e576532709819ce36ac75f9fe05
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-24 16:42:42 -0500

    added class to configure model mapper

--- src/main/java/com/example/wit/config/ConfigInstances.java
+++ src/main/java/com/example/wit/config/ConfigInstances.java
@@ -1,2 +1,13 @@
-package com.example.wit.config;public class ConfigInstances {
+package com.example.wit.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigInstances {
+    @Bean
+    public ModelMapper instanceModelMapper() {
+        return new ModelMapper();
+    }
 }

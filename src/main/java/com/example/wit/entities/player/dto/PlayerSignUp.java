503b5f00e6ee8fc67e41ca19eb1419a4470ef8f0
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-24 16:48:53 -0500

    added some annotations for validation

--- src/main/java/com/example/wit/entities/player/dto/PlayerSignUp.java
+++ src/main/java/com/example/wit/entities/player/dto/PlayerSignUp.java
@@ -1,19 +1,23 @@
 package com.example.wit.entities.player.dto;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Data
 @Getter @Setter @RequiredArgsConstructor
 public class PlayerSignUp {
+    @Size(max = 24)
+    @NotBlank
     final private String username;
     final private Integer points;
     final private Integer rating;
     final private Short currentCategory;
     final private Short bestCategory;
+    @NonNull
     final private Short careerId;
+    @NonNull
     final private Short universityId;
+    @NonNull
     final private Long teamId;
 }

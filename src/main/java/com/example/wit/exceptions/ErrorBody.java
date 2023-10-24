3d94173b085a11d73cea91b4b02a8f746205df84
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-24 16:53:56 -0500

    added class to homogenize exception bodies

--- src/main/java/com/example/wit/exceptions/ErrorBody.java
+++ src/main/java/com/example/wit/exceptions/ErrorBody.java
@@ -4,11 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class ErrorMessage {
-    List<Error> errors;
+public class ErrorBody {
+    List<String> errors;
 }

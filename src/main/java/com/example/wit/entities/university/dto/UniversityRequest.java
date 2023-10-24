ab711ff9ee67c64f73b74143deb2aeed99320450
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-24 17:54:02 -0500

    added dtos for request and response (uni)

--- src/main/java/com/example/wit/entities/university/dto/UniversityRequest.java
+++ src/main/java/com/example/wit/entities/university/dto/UniversityRequest.java
@@ -1,2 +1,17 @@
-package com.example.wit.entities.university.dto;public class UniversityRequest {
+package com.example.wit.entities.university.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Getter @RequiredArgsConstructor
+public class UniversityRequest {
+    @Size(max = 6)
+    final private String acronym;
+    @Size(max = 50)
+    @NotBlank
+    final private String fullName;
 }
--- src/main/java/com/example/wit/entities/university/dto/UniversityResponse.java
+++ src/main/java/com/example/wit/entities/university/dto/UniversityResponse.java
@@ -1,2 +1,13 @@
-package com.example.wit.entities.university.dto;public class UniversityResponse {
+package com.example.wit.entities.university.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Getter @RequiredArgsConstructor
+public class UniversityResponse {
+    final private Short id;
+    final private String acronym;
+    final private String fullName;
 }

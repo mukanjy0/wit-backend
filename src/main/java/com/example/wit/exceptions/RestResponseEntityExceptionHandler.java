3938b2bdebd2e136a67079f81e0e8dc5d9b5d7d8
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-24 16:54:19 -0500

    added global exception handler

--- src/main/java/com/example/wit/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/wit/exceptions/RestResponseEntityExceptionHandler.java
@@ -1,2 +1,41 @@
-package com.example.wit.exceptions;public class RestResponseEntityExceptionHandler {
+package com.example.wit.exceptions;
+
+import com.example.wit.entities.player.exceptions.PlayerNotFoundException;
+import jakarta.annotation.Nullable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+//    @ExceptionHandler(value = PlayerNotFoundException.class)
+//    protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+//        String body = "something";
+//        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+//    }
+
+    @ExceptionHandler(value = PlayerNotFoundException.class)
+    protected ResponseEntity<ErrorBody> handlePlayerNotFoundException(PlayerNotFoundException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        List<String> errors = Collections.singletonList(ex.getMessage());
+        return handleExceptionInternal(ex, new ErrorBody(errors), new HttpHeaders(), status, request);
+    }
+
+    protected ResponseEntity<ErrorBody> handleExceptionInternal(Exception ex, @Nullable ErrorBody body,
+                                                               HttpHeaders headers, HttpStatus status,
+                                                               WebRequest request) {
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
+        }
+
+        return new ResponseEntity<>(body, headers, status);
+    }
 }

4b57ea65615da5341b6f469562fb6cf864155e09
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-24 13:03:39 -0500

    added player not found exception class

--- src/main/java/com/example/wit/entities/player/exceptions/PlayerNotFoundException.java
+++ src/main/java/com/example/wit/entities/player/exceptions/PlayerNotFoundException.java
@@ -1,2 +1,17 @@
-package com.example.wit.entities.player.exceptions;public class PlayerNotFoundException {
+package com.example.wit.entities.player.exceptions;
+
+import lombok.AllArgsConstructor;
+
+public class PlayerNotFoundException extends RuntimeException{
+    final private String username;
+    public PlayerNotFoundException createWith(String username) {
+        return new PlayerNotFoundException(username);
+    }
+    private PlayerNotFoundException(String username) {
+        this.username = username;
+    }
+    @Override
+    public String getMessage() {
+        return "Player '" + username + "' not found";
+    }
 }

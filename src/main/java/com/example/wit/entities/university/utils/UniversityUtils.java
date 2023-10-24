422ac7fa54125fba9ebdeb6b474ec0bf1cc8ab36
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-24 17:54:31 -0500

    added utility class for aux methods used in service

--- src/main/java/com/example/wit/entities/university/utils/UniversityUtils.java
+++ src/main/java/com/example/wit/entities/university/utils/UniversityUtils.java
@@ -1,2 +1,14 @@
-package com.example.wit.entities.university.utils;public class UniversityUtils {
+package com.example.wit.entities.university.utils;
+
+import com.example.wit.entities.university.domain.University;
+import com.example.wit.entities.university.dto.UniversityRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniversityUtils {
+    public static University updateUniversity(University original, UniversityRequest toUpdate) {
+        if (toUpdate.getAcronym() != null) original.setAcronym(toUpdate.getAcronym());
+        if (toUpdate.getFullName() != null) original.setFullName(toUpdate.getFullName());
+        return original;
+    }
 }

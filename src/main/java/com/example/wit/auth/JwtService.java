c74a2732124db9ae6f5bd21f16fea9e8b535b859
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-25 09:41:23 -0500

    commented out classes

--- src/main/java/com/example/wit/auth/JwtService.java
+++ src/main/java/com/example/wit/auth/JwtService.java
@@ -2,26 +2,26 @@ package com.example.wit.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
-@Service
-public interface JwtService {
-    String jwtSigningKey = "${my.secret}";
-    Jwt jwt = new Jwt(jwtSigningKey);
-
-    public String extractUserName(String token) {
-        return jwt.extractClaim(token, Claims::getSubject);
-    }
-
-    public String generateToken(UserDetails userDetails) {
-        return jwt.generateToken(new HashMap<>(), userDetails);
-    }
-
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String userName = extractUserName(token);
-        return (userName.equals(userDetails.getUsername())) && !jwt.isTokenExpired(token);
-    }
-}
+//@Service
+//public interface JwtService {
+//    String jwtSigningKey = "${my.secret}";
+//    Jwt jwt = new Jwt(jwtSigningKey);
+//
+//    public String extractUserName(String token) {
+//        return jwt.extractClaim(token, Claims::getSubject);
+//    }
+//
+//    public String generateToken(UserDetails userDetails) {
+//        return jwt.generateToken(new HashMap<>(), userDetails);
+//    }
+//
+//    public boolean isTokenValid(String token, UserDetails userDetails) {
+//        final String userName = extractUserName(token);
+//        return (userName.equals(userDetails.getUsername())) && !jwt.isTokenExpired(token);
+//    }
+//}

1ee5e0d6e40694e8ce095dde99bf2fe8e4190c1f
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-25 09:41:34 -0500

    commented out config file

--- src/main/java/com/example/wit/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/wit/config/JwtAuthenticationFilter.java
@@ -3,7 +3,7 @@ package com.example.wit.config;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtS jewService;
-}
+//@Component
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//    private final JwtS jewService;
+//}

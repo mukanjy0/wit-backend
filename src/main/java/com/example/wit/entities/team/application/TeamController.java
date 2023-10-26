3ffabec8a309ffad88d05fc0a17d9cd54a8f5c6a
mukanjy0 <mukanjyo29@protonmail.com>
2023-10-26 00:08:49 -0500

    added controller with crud methods

--- src/main/java/com/example/wit/entities/team/application/TeamController.java
+++ src/main/java/com/example/wit/entities/team/application/TeamController.java
@@ -1,2 +1,39 @@
-package com.example.wit.entities.team.application;public class TeamController {
+package com.example.wit.entities.team.application;
+
+import com.example.wit.entities.team.domain.TeamService;
+import com.example.wit.entities.team.dto.TeamRequest;
+import com.example.wit.entities.team.dto.TeamResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+    @Autowired
+    private TeamService service;
+
+    @GetMapping
+    public ResponseEntity<List<TeamResponse>> read () {
+        return service.read();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<TeamResponse> read (@PathVariable Long id) {
+        return service.read(id);
+    }
+    @PostMapping
+    public ResponseEntity<String> create (@Valid @RequestBody TeamRequest team) {
+        return service.create(team);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update (@PathVariable Long id, @RequestBody TeamRequest team) {
+        return service.update(id, team);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete (@PathVariable Long id) {
+        return service.delete(id);
+    }
 }

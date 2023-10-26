1ad79854198e070b5fd0c6fb7d1c44e435203b9c
mukanjy0 <mukanjyo29@protonmail.com>
2023-10-25 23:06:22 -0500

    added utility class for service aux functions

--- src/main/java/com/example/wit/entities/career/utils/CareerUtils.java
+++ src/main/java/com/example/wit/entities/career/utils/CareerUtils.java
@@ -1,2 +1,14 @@
-package com.example.wit.entities.career.utils;public class CareerUtils {
+package com.example.wit.entities.career.utils;
+
+import com.example.wit.entities.career.domain.Career;
+import com.example.wit.entities.career.dto.CareerRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CareerUtils {
+    public static Career updateCareer(Career original, CareerRequest toUpdate) {
+        if (toUpdate.getName() != null) original.setName(toUpdate.getName());
+        if (toUpdate.getDescription() != null) original.setDescription(toUpdate.getDescription());
+        return original;
+    }
 }

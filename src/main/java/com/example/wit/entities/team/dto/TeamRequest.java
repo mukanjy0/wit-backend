999ebef15172195ac544d2b18faf5c6af57466b9
mukanjy0 <mukanjyo29@protonmail.com>
2023-10-26 00:08:12 -0500

    added dtos for team request & response

--- src/main/java/com/example/wit/entities/team/dto/TeamRequest.java
+++ src/main/java/com/example/wit/entities/team/dto/TeamRequest.java
@@ -1,2 +1,13 @@
-package com.example.wit.entities.team.dto;public class TeamRequest {
+package com.example.wit.entities.team.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class TeamRequest {
+    @Size(max = 24) @NonNull
+    private String name;
+    private Integer rating;
 }
--- src/main/java/com/example/wit/entities/team/dto/TeamResponse.java
+++ src/main/java/com/example/wit/entities/team/dto/TeamResponse.java
@@ -1,2 +1,13 @@
-package com.example.wit.entities.team.dto;public class TeamResponse {
+package com.example.wit.entities.team.dto;
+
+import lombok.*;
+
+@Data
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class TeamResponse {
+    private Long id;
+    private String name;
+    private Integer rank;
+    private Integer rating;
 }

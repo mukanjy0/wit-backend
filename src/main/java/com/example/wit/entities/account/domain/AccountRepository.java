42b70077f3429b76a26310d91b82419193a4d550
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-10-26 14:28:56 -0500

    added repo for account

--- src/main/java/com/example/wit/entities/account/domain/AccountRepository.java
+++ src/main/java/com/example/wit/entities/account/domain/AccountRepository.java
@@ -1,2 +1,13 @@
-package com.example.wit.entities.account.domain;public interface AccountRepository {
+package com.example.wit.entities.account.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findAccountByHandleAndPlatformId(String handle, Short platformId);
+    Boolean existsAccountByHandleAndPlatformId(String handle, Short platformId);
+    Boolean existsAccountByPlayerIdAndPlatformId(Long playerId, Short platformId);
 }

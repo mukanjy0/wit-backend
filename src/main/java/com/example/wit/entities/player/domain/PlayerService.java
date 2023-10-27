4413f859f934fc2a3e00f513169a14f056b23915
mukanjy0 <mukanjyo29@protonmail.com>
2023-10-27 12:31:08 -0500

    updated player repo & service to comply with enums & throw appropiate exceptions

--- src/main/java/com/example/wit/entities/player/domain/PlayerService.java
+++ src/main/java/com/example/wit/entities/player/domain/PlayerService.java
@@ -1,11 +1,17 @@
 package com.example.wit.entities.player.domain;
 
+import com.example.wit.entities.career.domain.Career;
 import com.example.wit.entities.career.domain.CareerRepository;
+import com.example.wit.entities.player.domain.category.Category;
+import com.example.wit.entities.player.domain.role.Role;
 import com.example.wit.entities.player.dto.PlayerResponse;
 import com.example.wit.entities.player.dto.PlayerSignUp;
 import com.example.wit.entities.player.dto.PlayerUpdate;
+import com.example.wit.entities.team.domain.Team;
 import com.example.wit.entities.team.domain.TeamRepository;
+import com.example.wit.entities.university.domain.University;
 import com.example.wit.entities.university.domain.UniversityRepository;
+import com.example.wit.exceptions.ElementAlreadyExistsException;
 import com.example.wit.exceptions.ElementNotFoundException;
 import com.example.wit.entities.player.utils.PlayerUtils;
 import jakarta.annotation.PostConstruct;
@@ -17,9 +23,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class PlayerService {
@@ -28,48 +36,77 @@ public class PlayerService {
     @Autowired
     private PlayerRepository repository;
     @Autowired
-    private UniversityRepository universityRepository;
+    private TeamRepository teamRepository;
     @Autowired
     private CareerRepository careerRepository;
     @Autowired
-    private TeamRepository teamRepository;
+    private UniversityRepository universityRepository;
 
-    public ResponseEntity<List<Player>> read () {
-        List<Player> players = repository.findAll();
+//    @PostConstruct
+//    private void configureModelMapper() {
+//    }
+
+    public ResponseEntity<List<PlayerResponse>> read () {
+        List<PlayerResponse> players = repository.findAll().stream().map(player -> mapper.map(player, PlayerResponse.class)).toList();
         return new ResponseEntity<>(players, HttpStatus.OK);
     }
 
-    public ResponseEntity<Player> read (Long id) throws ElementNotFoundException {
+    public ResponseEntity<PlayerResponse> read (Long id) throws ElementNotFoundException {
         Optional<Player>  player = repository.findById(id);
         if (player.isEmpty()) {
             throw ElementNotFoundException.createWith("Player", id.toString());
         }
 
-        return new ResponseEntity<>(player.get(), HttpStatus.OK);
+        return new ResponseEntity<>(mapper.map(player.get(), PlayerResponse.class), HttpStatus.OK);
     }
     public ResponseEntity<String> create (PlayerSignUp player) {
         Player newPlayer = mapper.map(player, Player.class);
-        newPlayer.setUniversity(universityRepository.findById(player.getUniversityId()).get());
-        newPlayer.setCareer(careerRepository.findById(player.getCareerId()).get());
-        newPlayer.setTeam(teamRepository.findById(player.getTeamId()).get());
-        newPlayer.setPassword("hello world");
+        newPlayer.setPassword(player.getPassword());
+        newPlayer.setBestCategory(newPlayer.getCurrentCategory());
         newPlayer.setRegistrationDate(LocalDate.now());
+
         repository.save(newPlayer);
+
         return ResponseEntity.status(201).body("Player created.");
     }
 
-    public ResponseEntity<String> update (Long id, PlayerUpdate p) throws ElementNotFoundException, IllegalArgumentException {
+    public ResponseEntity<String> update (Long id, PlayerUpdate player) throws ElementNotFoundException, IllegalArgumentException {
         Optional<Player> original = repository.findById(id);
         if (original.isEmpty()) {
             throw ElementNotFoundException.createWith("Player", id.toString());
         }
-        Player player = original.get();
-        try {
-            repository.save(PlayerUtils.updatePlayer(player, p));
+
+        Optional<Team> team = teamRepository.findById(player.getTeamId());
+        if (team.isEmpty()) {
+            throw ElementNotFoundException.createWith("Team", player.getTeamId().toString());
         }
-        catch (IllegalArgumentException ex) {
-            throw ex;
+
+        Optional<Career> career = careerRepository.findById(player.getCareerId());
+        if (career.isEmpty()) {
+            throw ElementNotFoundException.createWith("Career", player.getCareerId().toString());
         }
+
+        Optional<University> university = universityRepository.findById(player.getUniversityId());
+        if (university.isEmpty()) {
+            throw ElementNotFoundException.createWith("University", player.getUniversityId().toString());
+        }
+
+        Player previous = original.get();
+        String username = previous.getUsername();
+
+        Player updated = PlayerUtils.updatePlayer(previous, player);
+        String newUsername = updated.getUsername();
+
+        if (!username.equals(newUsername) && repository.existsPlayerByUsername(newUsername)) {
+            throw ElementAlreadyExistsException.createWith(newUsername, "username");
+        }
+
+        updated.setTeam(team.get());
+        updated.setCareer(career.get());
+        updated.setUniversity(university.get());
+
+        repository.save(updated);
+
         return ResponseEntity.status(200).body("Player with id " + id.toString() + " updated.");
     }
 

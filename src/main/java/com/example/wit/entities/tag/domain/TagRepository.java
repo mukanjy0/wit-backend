748cf16513101ee07da2e9d78a14546d10e2c5e4
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2023-11-10 15:29:34 -0500

    finished tag class & repo (needed for problem)

--- src/main/java/com/example/wit/entities/tag/domain/TagRepository.java
+++ src/main/java/com/example/wit/entities/tag/domain/TagRepository.java
@@ -1,2 +1,8 @@
-package com.example.wit.entities.tag.domain;public interface TagRepository {
+package com.example.wit.entities.tag.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Short> {
 }
